679dfd514d8f9459dc1a82b1d787457638929eae
Kevin Chung <kchung@nyu.edu>
2021-04-25 15:23:25 -0400

    Try downgrading setuptools-golang

--- setup.py
+++ setup.py
@@ -64,8 +64,8 @@ setuptools.setup(
     python_requires=">=3.5",
     # I'm not sure what this value is supposed to be
     build_golang={"root": "github.com/ColdHeat/pybluemonday"},
-    # ext_modules=[setuptools.Extension("pybluemonday/bluemonday", ["bluemonday.go"])],
-    # setup_requires=["setuptools-golang==2.3.0", "cffi~=1.1"],
+    ext_modules=[setuptools.Extension("pybluemonday/bluemonday", ["bluemonday.go"])],
+    setup_requires=["setuptools-golang==1.7.0", "cffi~=1.1"],
     setup_requires=["cffi~=1.1"],
     install_requires=["cffi~=1.1"],
 )
