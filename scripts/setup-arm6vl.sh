328a899b9763d2710b8876fb9ee945466ccee4e5
Kevin Chung <kchung@ctfd.io>
2022-02-16 11:58:09 -0500

    Add musl linux back into the build matrix (#32)
    * Add musllinux to the build matrix and try to get go running properly under alpine (Closes #31)
    * Fix building wheels for Python 3.10 (Closes #29)

--- scripts/setup-arm6vl.sh
+++ scripts/setup-arm6vl.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 if [ ! -f "/usr/bin/go" ]; then
     echo "Downloading arm6vl Golang"
-    curl -L -O https://golang.org/dl/go1.15.5.linux-armv6l.tar.gz
+    if [ -x /usr/bin/curl ]; then
+        curl -L -O https://golang.org/dl/go1.15.5.linux-armv6l.tar.gz
+    else
+        wget -v https://golang.org/dl/go1.15.5.linux-armv6l.tar.gz
+    fi
     tar -xf go1.15.5.linux-armv6l.tar.gz
+    if [ -x /lib/libc.musl-* ]; then
+        apk add --no-cache libc6-compat
+    fi
 fi
--- scripts/setup-macos.sh
+++ scripts/setup-macos.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 if [ ! -f "/usr/local/bin/go" ]; then
     echo "Downloading darwin-amd64 Golang"
-    curl -L -O https://golang.org/dl/go1.15.5.darwin-amd64.tar.gz
+    if [ -x /usr/bin/curl ]; then
+        curl -L -O https://golang.org/dl/go1.15.5.darwin-amd64.tar.gz
+    else
+        wget -v https://golang.org/dl/go1.15.5.darwin-amd64.tar.gz
+    fi
     tar -xf go1.15.5.darwin-amd64.tar.gz
 fi
