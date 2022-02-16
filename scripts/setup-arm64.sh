7e4a96d795c3cdea24dc22349edf62b6ab3602b1
Kevin Chung <kchung@ctfd.io>
2022-02-16 09:53:46 -0500

    Add musl linux back into the build matrix

--- scripts/setup-arm64.sh
+++ scripts/setup-arm64.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 if [ ! -f "/usr/bin/go" ]; then
     echo "Downloading arm64 Golang"
-    curl -vvv -L -O https://golang.org/dl/go1.15.5.linux-arm64.tar.gz
+    if [ -x /usr/bin/curl ]; then
+        curl -L -O https://golang.org/dl/go1.15.5.linux-amd64.tar.gz
+    else
+        wget https://golang.org/dl/go1.15.5.linux-amd64.tar.gz
+    fi
     tar -xf go1.15.5.linux-arm64.tar.gz
+    if [ -x /lib/libc.musl-* ]; then
+        apk add --no-cache libc6-compat
+    fi
 fi
