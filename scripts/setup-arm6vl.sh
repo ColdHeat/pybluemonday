ee39770b5180b9c840130f291e4c6118ae5b8088
Kevin Chung <kchung@ctfd.io>
2022-02-16 10:04:58 -0500

    Add verbosity to go download script wget

--- scripts/setup-arm6vl.sh
+++ scripts/setup-arm6vl.sh
@@ -4,7 +4,7 @@ if [ ! -f "/usr/bin/go" ]; then
     if [ -x /usr/bin/curl ]; then
         curl -L -O https://golang.org/dl/go1.15.5.linux-amd64.tar.gz
     else
-        wget https://golang.org/dl/go1.15.5.linux-amd64.tar.gz
+        wget -v https://golang.org/dl/go1.15.5.linux-amd64.tar.gz
     fi
     tar -xf go1.15.5.linux-armv6l.tar.gz
     if [ -x /lib/libc.musl-* ]; then
--- scripts/setup-macos.sh
+++ scripts/setup-macos.sh
@@ -4,7 +4,7 @@ if [ ! -f "/usr/local/bin/go" ]; then
     if [ -x /usr/bin/curl ]; then
         curl -L -O https://golang.org/dl/go1.15.5.linux-amd64.tar.gz
     else
-        wget https://golang.org/dl/go1.15.5.linux-amd64.tar.gz
+        wget -v https://golang.org/dl/go1.15.5.linux-amd64.tar.gz
     fi
     tar -xf go1.15.5.darwin-amd64.tar.gz
 fi
