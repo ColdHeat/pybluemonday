e906745c4a6916066ccf1ed77c6621cf39c175fd
Kevin Chung <kchung@ctfd.io>
2024-09-26 05:15:37 +0000

    Bump golang version

--- scripts/setup-arm6vl.sh
+++ scripts/setup-arm6vl.sh
@@ -2,11 +2,11 @@
 if [ ! -f "/usr/bin/go" ]; then
     echo "Downloading arm6vl Golang"
     if [ -x /usr/bin/curl ]; then
-        curl -L -O https://golang.org/dl/go1.15.5.linux-armv6l.tar.gz
+        curl -L -O https://golang.org/dl/go1.23.1.linux-armv6l.tar.gz
     else
-        wget -v https://golang.org/dl/go1.15.5.linux-armv6l.tar.gz
+        wget -v https://golang.org/dl/go1.23.1.linux-armv6l.tar.gz
     fi
-    tar -xf go1.15.5.linux-armv6l.tar.gz
+    tar -xf go1.23.1.linux-armv6l.tar.gz
     if [ -x /lib/libc.musl-* ]; then
         apk add --no-cache libc6-compat
     fi
--- scripts/setup-linux-64.sh
+++ scripts/setup-linux-64.sh
@@ -2,11 +2,11 @@
 if [ ! -f "/usr/bin/go" ]; then
     echo "Downloading linux-amd64 Golang"
     if [ -x /usr/bin/curl ]; then
-        curl -L -O https://golang.org/dl/go1.15.5.linux-amd64.tar.gz
+        curl -L -O https://golang.org/dl/go1.23.1.linux-amd64.tar.gz
     else
-        wget -v https://golang.org/dl/go1.15.5.linux-amd64.tar.gz
+        wget -v https://golang.org/dl/go1.23.1.linux-amd64.tar.gz
     fi
-    tar -xf go1.15.5.linux-amd64.tar.gz
+    tar -xf go1.23.1.linux-amd64.tar.gz
     if [ -x /lib/libc.musl-* ]; then
         apk add --no-cache libc6-compat
     fi
